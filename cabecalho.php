5fc717636fe2f408a16177743f2d9f4d48c3d5a0
guiferrera <d-igo-guil@hotmail.com>
2017-01-20 16:05:18 -0200

    Commit 2
    Construção page index

---
+++ cabecalho.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/estilos.css">
+<meta name="LR7 Marketing Multimídia">
+<title>LR7 Marketing Multimídia</title>
+
+<header class="container">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <h1>LR7 Multimídia Marketing</h1>
+        <nav class="menu-opcoes menu-cabecalho">
+            <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Orçamentos</a></li>
+                <li><a href="sobre.php">Quem somos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+</header>
